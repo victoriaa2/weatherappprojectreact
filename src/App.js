b53c4a837eaeca2f757447644595eea9250a7bcc
Victoria Adeleke <adeleke-victoria@hotmail.co.uk>
2024-12-16 13:41:42 +0000

    Added axios integration so it shows the weather of a city

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Weather />
+
+      <div className="App">
+        <div className="container">
+          <div class="Weather"></div>
+        </div>
+      </div>
+
+      <form className="search-form">
+        <div class="row">
+          <div class="col-9 ">
+            <input
+              placeholder="Enter a city.."
+              class="form-control search-input"
+              type="search"
+            />{" "}
+          </div>
+          <input type="submit" value="search" class="search-button" />
+        </div>
+      </form>
+      <br />
+      <br />
+
+      <div class="WeatherInfo">
+        <h1 className="current-city">London</h1>
+
+        <br />
+        <br />
+
+        <div className="temperatureinfo">
+          <p>19°C | Cloudy</p>
+        </div>
+        <img
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+          className="weather-app-icon"
+        />
+        <br />
+        <br />
+
+        <p className="Humiditydescription">
+          Humidity: <span id="humidity">41%</span> Wind:
+          <span id="wind-speed">2.06km/h</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </div>
+
+      <hr />
+      <p className="footer-notes">
+        Coded by
+        <a href="https://github.com/victoriaa2" target="_blank">
+          Victoria Adeleke 👩🏾‍💻
         </a>
-      </header>
+      </p>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+}
