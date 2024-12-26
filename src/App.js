9aad252ed353cb91eadc5ea5eb7f7510a9768c82
Victoria Adeleke <adeleke-victoria@hotmail.co.uk>
2024-12-26 21:41:56 +0000

    Added API intergration

--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
 import React from "react";
-import "./styles.css";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="App">
-        <div className="container">
-          <div class="Weather"></div>
-        </div>
+      <div className="container">
+        <Weather defaultCity="London" />
       </div>
-
-      <form className="search-form">
-        <div class="row">
-          <div class="col-9 ">
-            <input
-              placeholder="Enter a city.."
-              class="form-control search-input"
-              type="search"
-            />{" "}
-          </div>
-          <input type="submit" value="search" class="search-button" />
-        </div>
-      </form>
-
-      <br />
-      <br />
-
-      <div class="WeatherInfo">
-        <h1 className="current-city">London</h1>
-
-        <br />
-        <br />
-
-        <div className="temperatureinfo">
-          <p>19°C | Cloudy</p>
-        </div>
-        <img
-          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-          alt="weather-app-icon"
-        />
-
-        <p className="Humiditydescription">
-          Humidity: <span id="humidity">41%</span> Wind:
-          <span id="wind-speed">2.06km/h</span>
-        </p>
-        <br />
-        <br />
-      </div>
-
-      <hr />
-      <br />
-      <p className="footer-notes">
-        Coded by
-        <a
-          href="https://github.com/victoriaa2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Victoria Adeleke 👩🏾‍💻
-        </a>
-      </p>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: "",
+      description: response.data.condition.description,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="searchform">
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-1 gy-1 gx-1">
+              <input
+                type="submit"
+                value="search"
+                className="button-primary w-90"
+              />
+            </div>
+          </div>
+        </form>
+
+        <br />
+        <br />
+
+        <p> {weatherData.date} | Local Time 17:45 </p>
+
+        <br />
+        <br />
+
+        <h1 className="ciytitle">{weatherData.city}</h1>
+        <br />
+
+        <p className="text-capitalize"> {weatherData.description}</p>
+
+        <img src={weatherData.iconUrl} alt={weatherData.description} />
+
+        <div className="col-md-offset-3">
+          <span className="temperature">
+            {" "}
+            {Math.round(weatherData.temperature)}
+          </span>
+          <span className="unit">°C </span>
+        </div>
+
+        <br />
+        <br />
+
+        <div className="Humiditydescription">
+          <p>
+            Humidity:{weatherData.humidity}% Wind:{weatherData.wind}km/h
+          </p>
+
+          <br />
+          <br />
+          <br />
+          <hr />
+          <br />
+
+          <footer>
+            <p className="footer-notes">
+              Coded by
+              <a href="https://github.com/victoriaa2" target="_blank">
+                Victoria Adeleke
+              </a>
+              👩🏾‍💻 with HTML, CSS, Javascript, React and is
+              <a
+                href="https://github.com/victoriaa2/Weather-app-project-"
+                target="_blank"
+              >
+                {" "}
+                on GitHub
+              </a>
+              and
+              <a
+                href="https://dancing-daffodil-b8503c.netlify.app"
+                target="_blank"
+              >
+                hosted on Netlify
+              </a>
+            </p>
+          </footer>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "7c3280d84e3ff37at9daa2a38ab9o743";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+}
